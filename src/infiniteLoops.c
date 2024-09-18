3781afe723f691cb8888a835c12bb9961d28a343
Stephen <ehlert.stephenj#gmail.com>
2024-09-18 13:13:09 -0600

    added files to test loops

---
+++ src/infiniteLoops.c
@@ -0,0 +1,15 @@
+void mainTaskWhileLoop(char c) {
+    while(true) {
+        c = getchar();
+        if (c <= 'z' && c >= 'a') putchar(c - 32);
+        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
+        else putchar(c);
+    }
+}
+
+void  blinkTaskWhileLoop(int count, bool on) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+        if (count++ % 11) on = !on;
+        vTaskDelay(500);
+
+}
